c14aa21e709e373f16a7bf64ed173d364362ad81
jbeaudette3 <67081844+jbeaudette3@users.noreply.github.com>
2021-09-24 21:34:24 -0500

    Create brands.py

---
+++ brands.py
@@ -0,0 +1,11 @@
+# load data
+df_brands = pd.DataFrame(pd.read_json('/data/brands.json', lines = True))
+
+# normalize
+brands = pd.json_normalize(json.loads(df_brands.to_json(orient = 'records')))
+
+# nulls
+brands = brands.where(pd.notnull(brands), None)
+
+# save cleaned dataset
+brands.to_csv('/data/cleaned_brands.csv')
