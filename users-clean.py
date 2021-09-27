79be4798a64496c6cf58386e0410d662e6bff1d0
jbeaudette3 <67081844+jbeaudette3@users.noreply.github.com>
2021-09-27 15:45:49 -0500

    Update and rename users.py to users-clean.py

---
+++ users-clean.py
@@ -0,0 +1,15 @@
+# load data
+df = pd.DataFrame(pd.read_json('/content/drive/MyDrive/Fetch Interview/users.json', lines = True))
+
+# normalize
+flat_users = pd.json_normalize(json.loads(df.to_json(orient='records')))
+
+# nulls
+flat_users = flat_users.where(pd.notnull(flat_users), None)
+
+# convert unix milliseconds to human readable dates
+flat_users['createdDate2.$date'] = pd.to_datetime(flat_users['createdDate.$date'], unit = 'ms')
+flat_users['lastLogin2.$date'] = pd.to_datetime(flat_users['lastLogin.$date'], unit = 'ms')
+
+# save as tab-separated values
+flat_users.to_csv('/content/drive/MyDrive/Fetch Interview/cleaned_users.tsv', sep='\t')
