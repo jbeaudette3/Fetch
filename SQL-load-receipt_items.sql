e721b0a69a94023eeb0944b5aa2708b4639372fb
jbeaudette3 <67081844+jbeaudette3@users.noreply.github.com>
2021-09-27 15:53:52 -0500

    Create SQL-load-receipt_items.sql

---
+++ SQL-load-receipt_items.sql
@@ -0,0 +1,48 @@
+DROP TABLE `Fetch`.`receipt_item`;
+CREATE TABLE `Fetch`.`receipt_item`(
+	i INT NOT NULL
+	, recpt_no TEXT (50) NOT NULL
+	, `_id.$oid` TEXT (50) NOT NULL
+	, item_no INT(5) NOT NULL
+	, barcode INT
+	, description TEXT
+	, finalPrice FLOAT
+	, itemPrice FLOAT
+	, needsFetchReview TEXT
+	, partnerItemId INT (10)
+	, preventTargetGapPoints TEXT
+	, quantityPurchased TEXT
+	, userFlaggedBarcode INT
+	, userFlaggedNewItem TEXT
+	, userFlaggedPrice FLOAT
+	, userFlaggedQuantity TEXT
+	, needsFetchReviewReason TEXT
+	, pointsNotAwardedReason TEXT
+	, pointsPayerId VARCHAR(50)
+	, rewardsGroup TEXT
+	, rewardsProductPartnerId VARCHAR(50)
+	, userFlaggedDescription TEXT
+	, originalMetaBriteBarcode INT
+	, originalMetaBriteDescription TEXT 
+	, brandCode TEXT
+	, competitorRewardsGroup TEXT
+	, discountedItemPrice FLOAT
+	, originalReceiptItemText TEXT
+	, itemNumber INT
+	, originalMetaBriteQuantityPurchased INT
+	, pointsEarned FLOAT
+	, targetPrice INT
+	, competitiveProduct TEXT
+	, originalFinalPrice FLOAT
+	, originalMetaBriteItemPrice FLOAT
+	, deleted TEXT
+	, priceAfterCoupon FLOAT
+	, metabriteCampaignId TEXT
+	, PRIMARY KEY(i)
+);
+
+LOAD DATA LOCAL INFILE '/data/cleaned_receipt_items.tsv'
+INTO TABLE receipt_item
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
