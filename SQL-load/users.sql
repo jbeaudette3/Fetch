225c1a522f837637b4ad30717866df1d6ba71034
jbeaudette3 <67081844+jbeaudette3@users.noreply.github.com>
2021-10-01 20:48:48 -0500

    Update users.sql
    changed csv to tsv

--- SQL-load/users.sql
+++ SQL-load/users.sql
@@ -2,20 +2,19 @@ SET GLOBAL local_infile=1;
 
 DROP TABLE `Fetch`.`users`;
 CREATE TABLE `Fetch`.`users` (
-	i INT NOT NULL
-	, active VARCHAR(10)
+	active VARCHAR(10)
 	, role VARCHAR(50)
 	, signUpSource VARCHAR(50)
 	, state VARCHAR(5)
-	, _id VARCHAR(50) NOT NULL
-	, createDate FLOAT
-	, lastLoginDate FLOAT
-	, lastLogin FLOAT
-	, PRIMARY KEY (i)
+	, `_id.$oid` VARCHAR(50) NOT NULL
+	, `createDate.$date` FLOAT
+	, `lastLoginDate.$date` FLOAT
+	, `createDate2.$date` TEXT
+	, `lastLoginDate2.$date` TEXT
 );
 
-LOAD DATA LOCAL INFILE '/data/cleaned_users.csv'
+LOAD DATA LOCAL INFILE '/Users/Jeff-Beaudette/Desktop/interview/cleaned_users.tsv'
 INTO TABLE users
-FIELDS TERMINATED BY ','
+FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
