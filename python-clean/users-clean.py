1eade20c057101dd4c2e4bd5777176e52fe64496
jbeaudette3 <67081844+jbeaudette3@users.noreply.github.com>
2021-10-01 19:02:45 -0500

    Update users-clean.py
    index = False, dropped extra column

--- python-clean/users-clean.py
+++ python-clean/users-clean.py
@@ -1,5 +1,6 @@
 # load data
-df = pd.DataFrame(pd.read_json('/content/drive/MyDrive/Fetch Interview/users.json', lines = True))
+df = pd.DataFrame(pd.read_json('/content/drive/MyDrive/Fetch Interview/users.json',\
+                               lines = True))
 
 # normalize
 flat_users = pd.json_normalize(json.loads(df.to_json(orient='records')))
@@ -11,5 +12,10 @@ flat_users = flat_users.where(pd.notnull(flat_users), None)
 flat_users['createdDate2.$date'] = pd.to_datetime(flat_users['createdDate.$date'], unit = 'ms')
 flat_users['lastLogin2.$date'] = pd.to_datetime(flat_users['lastLogin.$date'], unit = 'ms')
 
+# drop null column
+flat_users = flat_users.drop('lastLogin', axis = 1)
+
 # save as tab-separated values
-flat_users.to_csv('/content/drive/MyDrive/Fetch Interview/cleaned_users.tsv', sep='\t')
+flat_users.to_csv('/content/drive/MyDrive/Fetch Interview/cleaned_users.tsv',\
+                  sep='\t',\
+                  index = False)
